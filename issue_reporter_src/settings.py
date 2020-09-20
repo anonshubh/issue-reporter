e71e663a073375128f8a4a03a86d983740f1b3fa
Shubh Pathak <shubhpathak07@gmail.com>
2020-09-20 20:51:39 +0530

    Fixed Smtp Api

--- issue_reporter_src/settings.py
+++ issue_reporter_src/settings.py
@@ -55,7 +55,7 @@ INSTALLED_APPS = [
 SENDGRID_API_KEY = os.getenv('SENDGRID_API_KEY')
 
 EMAIL_HOST = 'smtp.sendgrid.net'
-EMAIL_HOST_USER = 'teameziportal@gmail.com'
+EMAIL_HOST_USER = 'apikey'
 EMAIL_HOST_PASSWORD = SENDGRID_API_KEY
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
