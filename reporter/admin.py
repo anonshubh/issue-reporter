802b0fb30e70c4cbf34afe6a6d775d0bf8d60a36
Shubh Pathak <shubhpathak07@gmail.com>
2020-09-22 09:56:08 +0530

    Registered InfoList Model

--- reporter/admin.py
+++ reporter/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Report,Vote
+from .models import Report,Vote,InformationList
 
 admin.site.register(Report)
 admin.site.register(Vote)
+admin.site.register(InformationList)
