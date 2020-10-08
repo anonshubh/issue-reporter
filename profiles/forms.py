aa9e0cba784cf236a1a73c23743606f997cfc836
Shubh Pathak <shubhpathak07@gmail.com>
2020-10-08 13:36:05 +0530

    Improved Rollno Restrictions

--- profiles/forms.py
+++ profiles/forms.py
@@ -44,7 +44,26 @@ class RestrictEmailAdapter(DefaultAccountAdapter):
             raise ValidationError('You are restricted from registering!')
         index = email.find('@')
         dot = email.find('.')
+        email = email.lower()
+        rollno = email[0:9]
+        if(len(rollno)!=9):
+            raise ValidationError("Only Students Rollno Accepted!")
+        branches = ("mdm","mfd","mpd","msm","coe","ced","edm","evd","esd")
+        name_valid = email[0:3]
+        year_valid = email[3:5]
+        roll_valid = email[6:9]
+        try:
+            if(not name_valid in branches):
+                raise ValidationError("Only Students Rollno Accepted!")
+            year_valid=int(year_valid)
+            roll_valid=int(roll_valid)
+        except:
+                raise ValidationError("Only Students Rollno Accepted!")
         domain = email[index+1:dot]
+        b_or_i = email[5:6]
+        b_or_i = b_or_i.lower()
+        if(b_or_i!='i' and b_or_i!='b'):
+            raise ValidationError("No Mtech/PhD supported, Contact Admin!")
         if(domain=='gmail'):
             raise ValidationError("Use only Official Institute Email ID!")
         domain_qs = Institute.objects.filter(domain=domain)
