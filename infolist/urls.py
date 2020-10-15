e259111c9e9f820892aa9f0f9725ea31ae2212a8
Shubh Pathak <shubhpathak07@gmail.com>
2020-10-15 11:16:13 +0530

    Added InfoList-Subject Add View

--- infolist/urls.py
+++ infolist/urls.py
@@ -5,4 +5,5 @@ app_name = 'infolist'
 
 urlpatterns=[
     path('',views.info_list_display_view,name='list'),
+    path('add-subject/',views.info_list_add_subject_view,name='add-subject'),
 ]
--- infolist/views.py
+++ infolist/views.py
@@ -1,15 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
+from django.core.exceptions import PermissionDenied
+from django.contrib import messages
 
 from .models import InfoList,Subject
 from .forms import InfoListForm,SubjectForm
 
 @login_required
 def info_list_display_view(request):
-    subject = Subject.objects.filter(department=request.user.info.department,year=request.user.info.join_year)
+    subjects = Subject.objects.filter(department=request.user.info.department,year=request.user.info.join_year)
     qs = InfoList.objects.filter(department=request.user.info.department,year=request.user.info.join_year)
     data = []
-    for i in subject:
+    for i in subjects:
         objects = qs.filter(subject=i)
         data.append(object)
     return render(request,'infolist/list.html',{'object_list':data})
+
+@login_required
+def info_list_add_subject_view(request):
+    if(not request.user.info.is_cr):
+        raise PermissionDenied
+    form = SubjectForm()
+    subjects = Subject.objects.filter(department=request.user.info.department,year=request.user.info.join_year)
+    if request.method=='POST':
+        form = SubjectForm(request.POST)
+        if(form.is_valid()):
+            subject = request.POST.get("subject")
+            sub_obj,created = Subject.objects.get_or_create(department=request.user.info.department,year=request.user.info.join_year,subject=subject)
+            if created:
+                messages.success(request,"Subject Added!")
+            else:
+                messages.info(request,"Subject Already Exists!")
+        else:
+            messages.error(request,"Form is Invalid!")
+        return redirect('infolist:add-subject')
+    return render(request,'infolist/add-subject.html',{'form':form,'subjects':subjects})
